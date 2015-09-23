5feeecd3071b5da61a6e709b027c485bed700cd7
Joseph Kordish <joseph.kordish@mandiant.com>
2015-09-23 10:46:57 -0500

    make get module moar better

--- src/mods/get.rs
+++ src/mods/get.rs
@@ -28,27 +28,23 @@ pub fn get(server: &str, port: u16, key: &str, verbose: bool ) {
         return;
     }
 
-    // store the body of the return message
-    let body: &str = std::str::from_utf8(resp.get_body()).unwrap();
-
     // verbose: print out the response code, headers, and body
     if verbose {
         println!("code={}; headers={:?}; body={:?}",
-            resp.get_code(), resp.get_headers(), &body);
+            resp.get_code(), resp.get_headers(), std::str::from_utf8(resp.get_body()).unwrap());
     }
 
     // create a vector containg the body
-    let json = vec![&body[..]];
-
-    // if verbose: print out the data from the json vec
-    if verbose {
-        println!("json: {}", json[0]);
-    }
+    let body = vec![resp.get_body()];
 
     // not sure why but doing inter over json works
-    for x in json {
+    for x in body {
+        // if verbose: print out the data from the json vec
+        if verbose {
+            println!("json: {}", std::str::from_utf8(x).unwrap());
+        }
         // decode json to the ValueData struct
-        let deserialized: Vec<ValueData> = ::serde_json::from_str(&x).unwrap();
+        let deserialized: Vec<ValueData> = ::serde_json::from_slice(x).unwrap();
         // convert ValueData.Value from base64 utf8
         let value = deserialized[0].Value[..].to_owned().from_base64().unwrap();
         // Print the string of value
