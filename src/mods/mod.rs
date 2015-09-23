6f935c2a993a750b0bafbd02bf16ad111d4eabfc
Joseph Kordish <joseph.kordish@mandiant.com>
2015-09-23 10:46:57 -0500

    make get module moar better

--- src/mods/mod.rs
+++ src/mods/mod.rs
@@ -1,5 +1,7 @@
 pub mod get;
 pub mod put;
+pub mod list;
+use std::fmt;
 
 // struct that mirrors the json values from consul
 // using snake case as #[serde(alias="")] wouldn't work for me
@@ -12,4 +14,21 @@ pub struct ValueData {
   pub Key: String,
   pub Flags: i32,
   pub Value: String,
+}
+
+impl fmt::Display for ValueData {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+        write!(f, "{} {} {} {} {} {}",
+            self.CreateIndex,
+            self.ModifyIndex,
+            self.LockIndex,
+            self.Key,
+            self.Flags,
+            self.Value)
+    }
 }
